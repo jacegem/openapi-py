111812699934aa426a0053e4d965b7d7c02640a0
yoonsungwon <specialist.yoon@gmail.com>
2017-06-27 09:33:13 +0900

    DB create 문 변경
    가져오는 데이터 형식에 따라서 int, datetime 형식으로 변경

--- Database/table_create.sql
+++ Database/table_create.sql
@@ -1,12 +1,12 @@
-CREATE TABLE `pystock.opt10081` (
-  `종목코드` varchar(40) NOT NULL,
-  `현재가` varchar(40) DEFAULT NULL,
-  `거래량` varchar(40) DEFAULT NULL,
-  `거래대금` varchar(40) DEFAULT NULL,
-  `일자` varchar(40) NOT NULL,
-  `시가` varchar(40) DEFAULT NULL,
-  `고가` varchar(40) DEFAULT NULL,
-  `저가` varchar(40) DEFAULT NULL,
+CREATE TABLE `opt10081` (
+  `종목코드` varchar(6) NOT NULL,
+  `현재가` int(11) NOT NULL,
+  `거래량` int(11) NOT NULL,
+  `거래대금` int(11) NOT NULL,
+  `일자` datetime NOT NULL,
+  `시가` int(11) NOT NULL,
+  `고가` int(11) NOT NULL,
+  `저가` int(11) NOT NULL,
   `수정주가구분` varchar(40) DEFAULT NULL,
   `수정비율` varchar(40) DEFAULT NULL,
   `대업종구분` varchar(40) DEFAULT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `pystock.opt10081` (
   PRIMARY KEY (`종목코드`,`일자`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
-CREATE TABLE `pystock.opt10079` (
+CREATE TABLE `opt10079` (
   `종목코드` varchar(40) NOT NULL,
   `현재가` varchar(40) DEFAULT NULL,
   `거래량` varchar(40) DEFAULT NULL,
